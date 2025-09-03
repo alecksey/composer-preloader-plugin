ff2ef5ac20be9583f65787c7f01c11efee2db994
Oleksii Bogatchenko <delphiworld@gmail.com>
2025-09-03 10:16:00 +0300

    Added missed cache info

---
+++ src/Composer/PreloadGenerator/CacheInfo.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * @date
+ * @author Oleksii Bogatchenko
+ * @package composer-preloader-plugin
+ * @version 1.0
+ */
+
+namespace Oleksii\ComposerPreloader\Composer\PreloadGenerator;
+
+class CacheInfo {
+
+    protected string $fileListPathName;
+
+    public function __construct(string $fileListPathName)
+    {
+        $this->fileListPathName = $fileListPathName;
+    }
+
+    public function getMissedList() : array
+    {
+        if(!file_exists($this->fileListPathName)) {
+            throw new \InvalidArgumentException(
+                'File list not found'
+            );
+        }
+
+        $fileList = include $this->fileListPathName;
+
+        $status = opcache_get_status();
+
+        $missedList = [];
+        foreach ($status['scripts'] as $script) {
+            if(!in_array($script, $fileList)) {
+                $missedList[] = $script;
+            }
+        }
+
+        return $missedList;
+    }
+}
